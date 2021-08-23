c126154cf09c5403f4da32795060f536383d4b85
Mat <up2011287@myport.ac.uk>
2021-08-23 19:21:52 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Bayes__Theorem_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.displayResults = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PABox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PBABox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PBBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(70, 128);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // displayResults
+            // 
+            this.displayResults.Location = new System.Drawing.Point(185, 36);
+            this.displayResults.Name = "displayResults";
+            this.displayResults.ReadOnly = true;
+            this.displayResults.Size = new System.Drawing.Size(223, 20);
+            this.displayResults.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Probability:";
+            // 
+            // PABox
+            // 
+            this.PABox.Location = new System.Drawing.Point(54, 36);
+            this.PABox.Name = "PABox";
+            this.PABox.Size = new System.Drawing.Size(100, 20);
+            this.PABox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "P(A):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "P(B|A):";
+            // 
+            // PBABox
+            // 
+            this.PBABox.Location = new System.Drawing.Point(54, 63);
+            this.PBABox.Name = "PBABox";
+            this.PBABox.Size = new System.Drawing.Size(100, 20);
+            this.PBABox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "P(B):";
+            // 
+            // PBBox
+            // 
+            this.PBBox.Location = new System.Drawing.Point(54, 93);
+            this.PBBox.Name = "PBBox";
+            this.PBBox.Size = new System.Drawing.Size(100, 20);
+            this.PBBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Enter numbers in % form:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 178);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PBBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PBABox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PABox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.displayResults);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox displayResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PABox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PBABox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PBBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bayes__Theorem_Solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            displayResults.Clear();
+            try
+            {
+                float PA = float.Parse(PABox.Text);
+                float PBA = float.Parse(PBABox.Text);
+                float PB = float.Parse(PBBox.Text);
+            }
+            catch (FormatException ex)
+            {
+                //throw;
+                MessageBox.Show("Please Enter a valid number !","ERROR");
+            }
+
+            float numPA = float.Parse(PABox.Text);
+            float numPBA = float.Parse(PBABox.Text);
+            float numPB = float.Parse(PBBox.Text);
+            if (numPA > 100 || numPBA > 100 || numPB > 100)
+            {
+                displayResults.Text += "% cannot be greater then 100%";
+            }
+            else
+            {
+                float probability = (numPA * numPBA) / numPB;
+
+                displayResults.Text += probability.ToString();
+            }
+            
+        }
+    }
+}
